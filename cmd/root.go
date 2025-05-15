3582d15af49b5e78aeab329899d40a13b62b64bb
simonwehle <simon.wehle@gmx.net>
2025-05-15 21:39:51 +0200

    fix: version 1.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 )
 
 const toolName = "yt2abs"
-const version = "1.0.0"
+const version = "1.1.0"
 
 func printHelp() {
 	fmt.Printf("Usage: %s -a <ASIN> [-i <InputAudio>] [-c <ChaptersFile>] [-o <OutputLocation>]\n", toolName)
