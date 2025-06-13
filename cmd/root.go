5e2ec55c7a9863ccae8df824d8b5dd2cbd591113
simonwehle <simon.wehle@gmx.net>
2025-06-13 16:59:53 +0200

    fix: update -h

--- cmd/root.go
+++ cmd/root.go
@@ -17,29 +17,50 @@ const toolName = "yt2abs"
 const version = "1.3.0"
 
 func printHelp() {
-	fmt.Printf("Usage: %s [-a <ASIN>] | [-i <InputAudio>] [-c <ChaptersFile>] | [--nc] [-o <OutputLocation>]\n", toolName)
-	fmt.Println("\nOptions:")
-	fmt.Println("  -a <ASIN>         Audible ASIN (e.g., B07KKMNZCH) - Required unless -i is used")
-	fmt.Println("  -i <InputAudio>   Path to the MP3 file (default: audiobook.mp3)")
-	fmt.Println("  -c <ChaptersFile> Path to the chapters file (default: chapters.txt)")
-	fmt.Println("  -o <OutputPath>   Path to output location")
-	fmt.Println("  -h                Show this help message")
-	fmt.Println("  -v                Show version")
+	fmt.Printf("Usage:\n")
+	fmt.Printf("  %s metadata [input] [chapters] [output]\n\n", toolName)
+
+	fmt.Println("Sections and Options:")
+
+	fmt.Println("\n  metadata (required)")
+	fmt.Println("    -a <ASIN>         Audible ASIN (e.g., B07KKMNZCH)")
+	fmt.Println("    -t <Title>        Title of the output audiobook")
+
+	fmt.Println("\n  input")
+	fmt.Println("    -i <InputAudio>   Path to the MP3 file (default: audiobook.mp3)")
+	fmt.Println("    -f <InputFolder>  Path to input folder (disables chapter processing)")
+
+	fmt.Println("\n  chapters (optional)")
+	fmt.Println("    -c <ChaptersFile> Path to the chapters file (default: chapters.txt)")
+	fmt.Println("                      Chapters are skipped if no file is provided.")
+
+	fmt.Println("\n  output")
+	fmt.Println("    -o <OutputPath>   Path to output folder (default: current directory)")
+
+	fmt.Println("\n  misc")
+	fmt.Println("    -h                Show this help message")
+	fmt.Println("    -v                Show version")
+
 	fmt.Println("\nExamples:")
-	fmt.Println("  yt2abs -a B07KKMNZCH                 Full auto ASIN mode")
-	fmt.Println("  yt2abs -i \"My Book.mp3\"          Only MP3 to M4B no metadata and chapters")
-	fmt.Println("  yt2abs -a B07KKMNZCH -o \"/output/path\" Define output folder")
+	fmt.Println("  yt2abs -a B07KKMNZCH                    Full auto ASIN mode")
+	fmt.Println("  yt2abs -t \"My Audiobook\"                Only MP3 to M4B")
+	fmt.Println("  yt2abs -a B07KKMNZCH -o \"/output/path\"  Define output folder")
 }
 
 func Execute() {
 	asin := flag.String("a", "", "Audible ASIN (e.g., B07KKMNZCH)")
 	title := flag.String("t", "", "Title of the output audiobook")
 	audioFile := flag.String("i", "audiobook.mp3", "Path to the MP3 file")
-	inputFolder := flag.String("f", "", "Path to input Folder")
+	inputFolder := flag.String("f", "", "Path to input folder")
 	chapterFile := flag.String("c", "chapters.txt", "Path to the chapters file")
 	outputFolder := flag.String("o", "", "Path to output location")
 	showHelp := flag.Bool("h", false, "Show this help message")
 	showVersion := flag.Bool("v", false, "Show version")
+
+	if len(os.Args) == 1 {
+		printHelp()
+		os.Exit(0)
+	}
 
 	flag.Parse()
 
